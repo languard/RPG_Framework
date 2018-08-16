c5dc074cce45d811fb957eddbceb12b0b7563d46
Languard Gemstrike <languard@gmail.com>
2018-08-16 06:08:50 -0500

    no message

---
+++ Assets/RPG System/Scripts/Character and Party/Party.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.RPG_System.Scripts.Character_and_Party
+{
+    class Party
+    {
+        List<Entity> characterList;
+    }
+}
