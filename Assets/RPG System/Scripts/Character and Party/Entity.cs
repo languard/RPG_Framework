a7d8f44fbc50b0c8119a2351d92b1eb81fc2e8a9
Languard <languard@gmail.com>
2018-08-21 13:18:09 -0500

    no message

--- Assets/RPG System/Scripts/Character and Party/Entity.cs
+++ Assets/RPG System/Scripts/Character and Party/Entity.cs
@@ -12,36 +12,81 @@ public class Entity
     public static int CUR = 0;
     public static int BASE = 1;
 
+
     public string name;
 
-    public int[] strength;
-    public int[] constitution;
-    public int[] willpower;
-    public int[] intelligence;
+    public int strength;
+    public int strengthBase;
+    public int constitution;
+    public int constitutionBase;
+    public int willpower;
+    public int willpowerBase;
+    public int intelligence;
+    public int intelligenceBase;
 
-    public int[] hitPoints;
-    public int[] mana;
-    public int[] endurance;
-    public int[] reaction;
+    public int hitPoints;
+    public int hitPointsBase;
+
+    public int mana;
+    public int manaBase;
+
+    public int endurance;
+    public int enduranceBase;
+
+    public int reaction;
+    public int reactionBase;
 
     public int entityLevel;     //can be used as a scale value.  For example, level 5 monster vs level 10 monster, or a spell that adds 3 levels to a player.
     
 
     public Entity()
     {
-        strength = new int[] { 1, 1 };
-        constitution = new int[] { 1, 1 };
-        willpower = new int[] { 1, 1 };
-        intelligence = new int[] { 1, 1 };
-        hitPoints = new int[] { 1, 1 };
-        mana = new int[] { 1, 1 };
-        endurance = new int[] { 1, 1 };
-        reaction = new int[] { 1, 1 };
+        strength = 1;
+        strengthBase = 1;
+        constitution = 1;
+        constitutionBase = 1;
+        willpower = 1;
+        willpowerBase = 1;
+        intelligence = 1;
+        intelligenceBase = 1;
+
+        CalculateAllStats();
 
         entityLevel = 1;
 
         name = "DEFAULT";
     }
 
+    public void CalculateAllStats()
+    {
+        CalculateEndurance();
+        CalculateHP();
+        CalculateMana();
+        CalculateReaction();
+    }
+
+    public void CalculateHP()
+    {
+        hitPointsBase = (int)(StaticData.HP_BASE + (StaticData.HP_CON_SCALE * constitution + StaticData.HP_STR_SCALE * strength) * StaticData.HP_STAT_SCALE);
+        if (hitPoints > hitPointsBase) hitPoints = hitPointsBase;
+    }
+
+    public void CalculateMana()
+    {
+        manaBase = (int)(StaticData.M_BASE + (StaticData.M_INT_SCALE * intelligence + StaticData.M_WIL_SCALE * willpower) * StaticData.M_STAT_SCALE);
+        if (mana > manaBase) mana = manaBase;
+    }
+
+    public void CalculateEndurance()
+    {
+        enduranceBase = (int)(StaticData.END_BASE + (StaticData.END_CON_SCALE * constitution + StaticData.END_WIL_SCALE * willpower) * StaticData.END_STAT_SCALE);
+        if (endurance > enduranceBase) endurance = enduranceBase;
+    }
+
+    public void CalculateReaction()
+    {
+        reactionBase = (int)(StaticData.R_BASE + (StaticData.R_INT_SCALE * intelligence + StaticData.R_STR_SCALE * strength) * StaticData.R_STAT_SCALE);
+        reaction = reactionBase;
+    }
 }
 
--- Assets/RPG System/Scripts/StaticData.cs
+++ Assets/RPG System/Scripts/StaticData.cs
@@ -12,5 +12,39 @@ public class StaticData
     public const int DIR_RIGHT = 2;
     public const int DIR_DOWN = 3;
 
+    //HP Calculation constants
+    public const float HP_STR_SCALE = 1;
+    public const float HP_CON_SCALE = 1;
+    public const float HP_BASE = 1;
+    public const float HP_STAT_SCALE = 1;
+    public const float HP_CL_SCALE = 1;
+
+    //Mana Calculation constants
+    public const float M_INT_SCALE = 1;
+    public const float M_WIL_SCALE = 1;
+    public const float M_BASE = 1;
+    public const float M_STAT_SCALE = 1;
+    public const float M_CL_SCALE = 1;
+
+    //Reaction Calculation constants
+    public const float R_INT_SCALE = 1;
+    public const float R_STR_SCALE = 1;
+    public const float R_BASE = 1;
+    public const float R_STAT_SCALE = 1;
+    public const float R_CL_SCALE = 1;
+
+    //Endurance Calculation constants
+    public const float END_CON_SCALE = 1;
+    public const float END_WIL_SCALE = 1;
+    public const float END_BASE = 1;
+    public const float END_STAT_SCALE = 1;
+    public const float END_CL_SCALE = 1;
+
+    //removing this, allowing it to be fixed, not calculated
+    public const float CL_STR_SCALE = 1;
+    public const float CL_CON_SCALE = 1;
+    public const float CL_INT_SCALE = 1;
+    public const float CL_WIL_SCALE = 1;
+
 }
 
