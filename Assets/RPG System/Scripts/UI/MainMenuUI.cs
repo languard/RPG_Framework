38e100e574d33b83f2257ef78fa7377896c0b9d7
languard <languard@gmail.com>
2019-10-30 23:24:10 -0500

    Fixes

--- Assets/RPG System/Scripts/UI/MainMenuUI.cs
+++ Assets/RPG System/Scripts/UI/MainMenuUI.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class MainMenuUI : MonoBehaviour {
 
+    public AudioClip mainMenuMusic;
+    public AudioSource audioSource;
 
     public void StartGame()
     {
         Animator coreStates = GameObject.Find("GameMaster").GetComponent<Animator>();
         coreStates.SetTrigger("StartNewGame");
+
+       if(mainMenuMusic != null)
+        {
+            audioSource.clip = mainMenuMusic;
+            audioSource.loop = true;
+            audioSource.Play();
+        }
     }
 }
